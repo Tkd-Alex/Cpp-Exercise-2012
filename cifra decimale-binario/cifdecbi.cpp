714435ce88c544240a666b845d27f3f9164a502a
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-04-22 21:30:56 +0200

    binary decimal

---
+++ cifra decimale-binario/cifdecbi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main (){
+    float nu,dec;
+    int bi,i;
+    cout<<"Inserisci il numero decimale da convertire in binario.\n";
+    cin>>nu;
+    //Parte intera binaria
+    bi=(int)nu;
+    while(bi!=0){
+    cout<<bi%2;
+    bi/=2;}    
+    //Parte decimale binaria
+     bi=(int)nu;
+     dec=(nu-bi);
+     cout<<" <-- Stampato al contrario! , ";
+     i=0;
+     while(i<5){
+        dec*=2;
+        cout<<(int)dec;
+        if((int)dec==1){
+        dec=dec-(int)dec;}    
+        i++;}
+        cout<<endl;
+    system("pause");}    
