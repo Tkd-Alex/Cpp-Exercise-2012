56342e1205d9850614f7ed8211a8f476b3f203dd
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-04-22 21:25:11 +0200

    Cat

---
+++ Gatto/gattomerda.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.c>
+using namespace std;
+int main (){
+int i,m;  
+
+cout<<"         /"<<(char)92<<"_/"<<(char)92<<" "<<endl;   
+cout<<"    ____/ o o "<<(char)92<<endl;
+cout<<"  /~____  =ø= /"<<endl;
+cout<<" (______)__m_m)"<<endl;
+
+m=0;
+while(m<1){
+for(i=0;i<99999999;i++);  
+
+gotoxy(10,1);
+cout<<"-";
+
+gotoxy(12,1);
+cout<<"-";
+
+for(i=0;i<99999999;i++);  
+gotoxy(10,1);
+cout<<"o";
+gotoxy(12,1);
+cout<<"o";}
+
+
+gotoxy(40,12);
+system("pause");
+}
